c960fa168c4a97c15ab36c450a0d822eccf357f3
Morri <m.postolaki656555@gmail.com>
2021-02-10 08:56:12 +0000

    .

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -2,11 +2,10 @@ package test;
 
 import domain.Exercise;
 
-public class TestResult {
-
-    public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+public class TestResult
+{
+    public static void main(String[] args)
+    {
+        
     }
 }
